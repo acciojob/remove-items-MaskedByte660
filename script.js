845f16413df560863ac93e83b967fef7871606f3
MaskedByte660 <codingtechinfo01@gmail.com>
2025-02-14 13:11:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//add event listener to input
+//then find the selected option using selectedIndex
+//remove it.
+document.querySelector("input").addEventListener("click",()=>{
+	let dropDown = document.querySelector("colorSelect");
+	dropDown.remove(dropDown.selectedIndex);
+})
